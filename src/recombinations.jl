4cd4ac4ef19dd0b8ab085dc6d08bce091960706e
Art <wildart@gmail.com>
2017-07-30 05:39:18 -0400

    fixed indent

--- src/recombinations.jl
+++ src/recombinations.jl
@@ -185,116 +185,115 @@ function pmx{T <: Vector}(v1::T, v2::T)
 end
 
 # Order crossover
-function ox1{T <: Integer}(v1::Vector{T}, v2::Vector{T})
-  s = length(v1)
-  from, to = rand(1:s, 2)
-  from, to = from > to ? (to, from)  : (from, to)
-  c1 = zeros(v1)
-  c2 = zeros(v2)
-  # Swap
-  c1[from:to] = v2[from:to]
-  c2[from:to] = v1[from:to]
-  # Fill in from parents
-  k = to+1 > s ? 1 : to+1 #child1 index
-  j = to+1 > s ? 1 : to+1 #child2 index
-  for i in vcat(to+1:s,1:from-1)
-    while in(v1[k],c1)
-      k = k+1 > s? 1 : k+1
+function ox1{T <: Vector}(v1::T, v2::T)
+    s = length(v1)
+    from, to = rand(1:s, 2)
+    from, to = from > to ? (to, from)  : (from, to)
+    c1 = zeros(v1)
+    c2 = zeros(v2)
+    # Swap
+    c1[from:to] = v2[from:to]
+    c2[from:to] = v1[from:to]
+    # Fill in from parents
+    k = to+1 > s ? 1 : to+1 #child1 index
+    j = to+1 > s ? 1 : to+1 #child2 index
+    for i in vcat(to+1:s,1:from-1)
+        while in(v1[k],c1)
+            k = k+1 > s? 1 : k+1
+        end
+        c1[i] = v1[k]
+        while in(v2[j],c2)
+            j = j+1 > s? 1 : j+1
+        end
+        c2[i] = v2[j]
     end
-    c1[i] = v1[k]
-    while in(v2[j],c2)
-      j = j+1 > s? 1 : j+1
-    end
-    c2[i] = v2[j]
-  end
-  return c1, c2
+    return c1, c2
 end
 
 # Cycle crossover
-function cx{T <: Integer}(v1::Vector{T}, v2::Vector{T})
-  s = length(v1)
-  c1 = zeros(v1)
-  c2 = zeros(v2)
+function cx{T <: Vector}(v1::T, v2::T)
+    s = length(v1)
+    c1 = zeros(v1)
+    c2 = zeros(v2)
 
-  f1 = true #switch
-  k = 1
-  while k > 0
-    idx = k
-    if f1
-      #cycle from v1
-      while c1[idx] == zero(T)
-        c1[idx] = v1[idx]
-        c2[idx] = v2[idx]
-        idx = inmap(v2[idx],v1,1,s)
-      end
-    else
-      #cycle from v2
-      while c2[idx] == zero(T)
-        c1[idx] = v2[idx]
-        c2[idx] = v1[idx]
-        idx = inmap(v1[idx],v2,1,s)
-      end
+    f1 = true #switch
+    k = 1
+    while k > 0
+        idx = k
+        if f1
+            #cycle from v1
+            while c1[idx] == zero(T)
+            c1[idx] = v1[idx]
+            c2[idx] = v2[idx]
+            idx = inmap(v2[idx],v1,1,s)
+            end
+        else
+            #cycle from v2
+            while c2[idx] == zero(T)
+            c1[idx] = v2[idx]
+            c2[idx] = v1[idx]
+            idx = inmap(v1[idx],v2,1,s)
+            end
+        end
+        f1 $= true
+        k = inmap(zero(T),c2,1,s)
     end
-    f1 $= true
-    k = inmap(zero(T),c2,1,s)
-  end
-  return c1,c2
+    return c1,c2
 end
 
 # Order-based crossover
-function ox2{T <: Integer}(v1::Vector{T}, v2::Vector{T})
-  s = length(v1)
-  c1 = copy(v1)
-  c2 = copy(v2)
+function ox2{T <: Vector}(v1::T, v2::T)
+    s = length(v1)
+    c1 = copy(v1)
+    c2 = copy(v2)
 
-  for i in 1:s
-    if rand(Bool)
-      idx1 = inmap(v2[i],v1,1,s)
-      idx2 = inmap(v1[i],v2,1,s)
-      c1[idx1] = zero(T)
-      c2[idx2] = zero(T)
+    for i in 1:s
+        if rand(Bool)
+            idx1 = inmap(v2[i],v1,1,s)
+            idx2 = inmap(v1[i],v2,1,s)
+            c1[idx1] = zero(T)
+            c2[idx2] = zero(T)
+        end
     end
-  end
 
-  for i in 1:s
-    if !in(v2[i],c1)
-      tmpin = inmap(zero(T),c1,1,s)
-      c1[tmpin] = v2[i]
+    for i in 1:s
+        if !in(v2[i],c1)
+            tmpin = inmap(zero(T),c1,1,s)
+            c1[tmpin] = v2[i]
+        end
+        if !in(v1[i],c2)
+            tmpin = inmap(zero(T),c2,1,s)
+            c2[tmpin] = v1[i]
+        end
     end
-    if !in(v1[i],c2)
-      tmpin = inmap(zero(T),c2,1,s)
-      c2[tmpin] = v1[i]
-    end
-  end
-  return c1,c2
+    return c1,c2
 end
 
 # Position-based crossover
-function pos{T <: Integer}(v1::Vector{T}, v2::Vector{T})
-  s = length(v1)
-  c1 = zeros(v1)
-  c2 = zeros(v2)
+function pos{T <: Vector}(v1::T, v2::T)
+    s = length(v1)
+    c1 = zeros(v1)
+    c2 = zeros(v2)
 
-  for i in 1:s
-    if rand(Bool)
-      c1[i] = v2[i]
-      c2[i] = v1[i]
+    for i in 1:s
+        if rand(Bool)
+            c1[i] = v2[i]
+            c2[i] = v1[i]
+        end
     end
-  end
 
-  for i in 1:s
-    if !in(v1[i],c1)
-      tmpin = inmap(zero(T),c1,1,s)
-      c1[tmpin] = v1[i]
+    for i in 1:s
+        if !in(v1[i],c1)
+            tmpin = inmap(zero(T),c1,1,s)
+            c1[tmpin] = v1[i]
+        end
+        if !in(v2[i],c2)
+            tmpin = inmap(zero(T),c2,1,s)
+            c2[tmpin] = v2[i]
+        end
     end
-    if !in(v2[i],c2)
-      tmpin = inmap(zero(T),c2,1,s)
-      c2[tmpin] = v2[i]
-    end
-  end
-  return c1,c2
+    return c1,c2
 end
-
 
 # Utils
 # =====
