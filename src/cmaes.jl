b6a432d9807244e118eedb3f205a5841c91b9548
Andrew Haselgrove <aahaselgrove@gmail.com>
2019-09-27 17:23:46 +1000

    Fix early stopping criterion

--- src/cmaes.jl
+++ src/cmaes.jl
@@ -72,7 +72,7 @@ function cmaes( objfun::Function, N::Int;
 
         # termination condition
         count += 1
-        if count == iterations || σ < tol
+        if count == iterations || σ <= tol
             break
         end
         verbose && println("BEST: $(fitpop[1]): $(σ)")
