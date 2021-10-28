9660ff4634f2517185ba06eee25ec8a6da712f26
Art Wild <wildart@gmail.com>
2021-10-28 13:17:30 -0400

    added a test with a constraint

--- test/knapsack.jl
+++ test/knapsack.jl
@@ -8,17 +8,44 @@
 
     result = Evolutionary.optimize(
         x -> -fitnessFun(x),
-        rand(Bool,length(mass)),
+        initpop,
         GA(
             selection = roulette,
             mutation = inversion,
             crossover = singlepoint,
-            mutationRate = 0.2,
-            crossoverRate = 0.5,
+            mutationRate = 0.9,
+            crossoverRate = 0.2,
+            ɛ = 0.1,                                # Elitism
+            populationSize = 100,
+        ));
+    println("GA:RLT:INV:SP (-objfun) => F: $(minimum(result)), C: $(Evolutionary.iterations(result))")
+    @test abs(Evolutionary.minimum(result)) == 21.
+    @test sum(mass .* Evolutionary.minimizer(result)) <= 20
+
+
+    # with a constraint
+    fitnessFun = n -> sum(utility .* n)
+
+    cf(n) = [ sum(mass .* n) ] # constraint function
+    lc   = [0] # lower bound for constraint function
+    uc   = [20]   # upper bound for constraint function
+    con = WorstFitnessConstraints(Int[], Int[], lc, uc, cf)
+
+    initpop = BitVector(rand(Bool,length(mass)))
+
+    result = Evolutionary.optimize(
+        x -> -fitnessFun(x), con,
+        initpop,
+        GA(
+            selection = roulette,
+            mutation = flip,
+            crossover = singlepoint,
+            mutationRate = 0.9,
+            crossoverRate = 0.1,
             ɛ = 0.1,                                # Elitism
             populationSize = 50,
         ));
-    println("GA:RLT:INV:SP (-objfun) => F: $(minimum(result)), C: $(Evolutionary.iterations(result))")
+    println("GA:RLT:FL:SP (-objfun) => F: $(minimum(result)), C: $(Evolutionary.iterations(result))")
     @test abs(Evolutionary.minimum(result)) == 21.
     @test sum(mass .* Evolutionary.minimizer(result)) <= 20
 end
